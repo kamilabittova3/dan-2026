fa99ad851bd21d0447f03fe445129a1625579561
kamilabittova3 <kamila.bittova@gmail.com>
2026-02-13 20:47:29 -0500

    Name change in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine-2026/',
+  base: '/dan-2026/',
   envPrefix: ['VITE_', 'EMAILJS_'],
   server: {
     port: 5173,
